ac3a5dc039f538f7e8e9cfc38b3d226cb3f050de
zinh8803 <ngoquocvinh2003@gmail.com>
2025-06-29 23:40:34 +0700

    fix page adminproduct,fix layout home

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\SoftDeletes;
 class Product extends Model
 {
+    use SoftDeletes;
     protected $fillable = ['name', 'description', 'price', 'image_url', 'size','status', 'category_id'];
 
     public function category()
