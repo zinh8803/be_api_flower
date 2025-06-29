2e8813e6ae58f49c621e461a813199e20342a1e7
zinh8803 <ngoquocvinh2003@gmail.com>
2025-06-29 23:40:34 +0700

    add softdelete product

--- app/Models/OrderDetail.php
+++ app/Models/OrderDetail.php
@@ -15,7 +15,7 @@ class OrderDetail extends Model
 
     public function product()
     {
-        return $this->belongsTo(Product::class);
+        return $this->belongsTo(Product::class)->withTrashed();
     }
     public function productSize()
     {
