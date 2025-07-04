5bbd4cb18faf0d3fc57d9e53952a5449a6bc4ef4
Ngo Quoc Vinh <vinhnq@hybrid-technologies.vn>
2025-07-04 08:23:52 +0700

    test server-v7

--- docker/startup.sh
+++ docker/startup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-# Start queue worker in background
-php artisan queue:work --sleep=3 --tries=3 &
-
-# Đợi 2-3 giây cho worker khởi động (tùy ý)
-sleep 3
-
-# Start web server (artisan serve hoặc nginx+php-fpm)
-php artisan serve --host=0.0.0.0 --port=10000
+if [ "$RUN_QUEUE" = "true" ]; then
+    # Chạy queue worker
+    php artisan queue:work --sleep=3 --tries=3
+else
+    # Chạy web server (artisan serve hoặc supervisor)
+    # php artisan serve --host=0.0.0.0 --port=10000
+    /usr/bin/supervisord
+fi
